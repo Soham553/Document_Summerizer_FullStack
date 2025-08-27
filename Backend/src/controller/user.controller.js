21e5f07a8c23fde67158c270fcb8e2f962ad111c
Soham553 <sakharesoham10@gmail.com>
2025-08-27 17:52:19 +0530

    Backend routs add

---
+++ Backend/src/controller/user.controller.js
@@ -0,0 +1,12 @@
+import { User } from '../models/user.model.js';
+
+
+export const createUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+    }
+    catch (error){
+        
+    }
+}
